dff88421f389ae60013d56af8b18fc7ee578aad4
ng-minh1 <nguyen.minhtri@outlook.fr>
2026-04-09 18:42:36 +0200

    Création de la méthode permettant de convertir les touches Z,Q,S,D choisit par le joueur en une direction et permettre au jeu de faire le déplacement

--- src/main/java/fr/l2info/controller/PieceController.java
+++ src/main/java/fr/l2info/controller/PieceController.java
@@ -1,10 +1,26 @@
 package fr.l2info.controller;
 
 
+import fr.l2info.enums.Direction;
+import fr.l2info.model.Taquin;
+
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class PieceController extends KeyAdapter{
 
+    private final Taquin modele;
 
+    public PieceController(Taquin modele) {
+        this.modele = modele;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        char c = e.getKeyChar();
+        Direction direction = Direction.getFromKey(c);
+        if (direction != null) {
+            modele.tryMovement(direction);
+        }
+    }
 }
