eff600ea0749c96f3787c68559916077542c87cc
MatysALL <matysallanet@gmail.com>
2026-04-10 08:58:34 +0200

    Création de la View CoupsJouesView.java :
    Instancie un Taquin, et l'ajoute au model écouteur.
    Permet de mettre à jour le nombre de coups jouer.

--- src/main/java/fr/l2info/view/CoupsJouesView.java
+++ src/main/java/fr/l2info/view/CoupsJouesView.java
@@ -1,6 +1,27 @@
 package fr.l2info.view;
 
-public class CoupsJouesView {
-    // Matys
-    // JLabel
+import fr.l2info.model.EcouteurModele;
+import fr.l2info.model.Taquin;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CoupsJouesView extends JPanel implements EcouteurModele {
+
+    private Taquin modele;
+    private JLabel label;
+
+    public CoupsJouesView(Taquin modele) {
+        this.modele = modele;
+        modele.addEcouteur(this);
+
+        label = new JLabel("Nombre de coups : 0");
+        label.setFont(new Font("Arial", Font.PLAIN, 14));
+        add(label);
+    }
+
+    @Override
+    public void modelMisAJour(Object source) {
+        label.setText("Nombre de coups : " + modele.getNbCoups());
+    }
 }
